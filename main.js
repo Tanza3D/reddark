c2af3555f25429c7c714e9babce632cd34eed7e0
Max DeVos <max@maxdevos.com>
2023-06-12 04:33:09 -0500

    cleanup: var->let, type equals, removed unnecessary returns

--- main.js
+++ main.js
@@ -3,9 +3,9 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-var request = require("./requests.js");
-var config = require("./config.js")
-var { exec } = require('child_process');
+const request = require("./requests.js");
+const config = require("./config.js");
+let {exec} = require('child_process');
 
 const io = new Server(server, {
     cors: {
@@ -38,16 +38,16 @@ const subreddits_src = {
 }
 const subreddits = {};
 async function appendList(url) {
-    var section = [];
-    var sectionname = "";
-    var data = await request.httpsGet(url);
+    let section = [];
+    let sectionname = "";
+    let data = await request.httpsGet(url);
     data = JSON.parse(data);
-    text = data['data']['content_md'];
+    let text = data['data']['content_md'];
     //console.log(text);
-    lines = text.split("\n");
-    for (var line of lines) {
+    let lines = text.split("\n");
+    for (let line of lines) {
         if (line.startsWith("##") && !line.includes("Please") && line.includes(":")) {
-            if (section != []) subreddits_src[sectionname] = section;
+            if (section !== []) subreddits_src[sectionname] = section;
             section = [];
             sectionname = line.replace("##", "");
         }
@@ -63,10 +63,10 @@ async function createList() {
     console.log("grabbed subreddits");
     //subreddits_src["30+ million:"].push("r/tanzatest")
 
-    for (var section in subreddits_src) {
+    for (let section in subreddits_src) {
         console.log(section);
         subreddits[section] = [];
-        for (var subreddit in subreddits_src[section]) {
+        for (let subreddit in subreddits_src[section]) {
             subreddits[section].push({
                 "name": subreddits_src[section][subreddit].replace("\n", "").replace("\r", ""),
                 "status": "public"
@@ -74,15 +74,14 @@ async function createList() {
         }
     }
     console.log(subreddits);
-    return;
 }
 
 
 
 firstCheck = false;
-var countTimeout = null;
+let countTimeout = null;
 io.on('connection', (socket) => {
-    if (firstCheck == false) {
+    if (firstCheck === false) {
         //console.log("sending loading");
         socket.emit("loading");
     } else {
@@ -94,7 +93,7 @@ io.on('connection', (socket) => {
         console.log('currently connected users: ' + io.engine.clientsCount);
     }, 500);
 });
-if (config.prod == true) {
+if (config.prod === true) {
     exec("rm /var/tmp/reddark.sock")
     server.listen("/var/tmp/reddark.sock", () => {
         console.log('listening on /var/tmp/reddark.sock');
@@ -105,16 +104,16 @@ if (config.prod == true) {
         console.log('listening on *:' + config.port);
     });
 }
-var checkCounter = 0;
+let checkCounter = 0;
 
 async function updateStatus() {
     //return;
-    var todo = 0;
-    var done = 0;
-    var delay = 0;
+    let todo = 0;
+    let done = 0;
+    let delay = 0;
     const stackTrace = new Error().stack
     checkCounter++;
-    var doReturn = false;
+    let doReturn = false;
     console.log("Starting check " + checkCounter + " with stackTrace: " + stackTrace);
     for (let section in subreddits) {
         for (let subreddit in subreddits[section]) {
@@ -141,46 +140,42 @@ async function updateStatus() {
                             console.log("Response is not JSON? We're probably getting blocked... - " + data);
                             return;
                         }
-                        var resp = JSON.parse(data);
-                        if (typeof (resp['message']) != "undefined" && resp['error'] == 500) {
+                        const resp = JSON.parse(data);
+                        if (typeof (resp['message']) != "undefined" && resp['error'] === 500) {
                             console.log("We're probably getting blocked... (500) - " + resp);
                             return;
                         }
-                        if (typeof (resp['reason']) != "undefined" && resp['reason'] == "private" && subreddits[section][subreddit].status != "private") {
+                        if (typeof (resp['reason']) != "undefined" && resp['reason'] === "private" && subreddits[section][subreddit].status !== "private") {
                             //console.log(subreddits[section][subreddit].status);
                             subreddits[section][subreddit].status = "private";
-                            if (firstCheck == false)
+                            if (firstCheck === false)
                                 io.emit("update", subreddits[section][subreddit]);
                             else
                                 io.emit("updatenew", subreddits[section][subreddit]);
 
-                        } else if (subreddits[section][subreddit].status == "private" && typeof (resp['reason']) == "undefined") {
+                        } else if (subreddits[section][subreddit].status === "private" && typeof (resp['reason']) == "undefined") {
                             console.log("updating to public with data:")
                             console.log(resp);
                             subreddits[section][subreddit].status = "public";
                             io.emit("updatenew", subreddits[section][subreddit]);
                         }
 
-                        if (done > (todo - 2) && firstCheck == false) {
+                        if (done > (todo - 2) && firstCheck === false) {
                             io.emit("subreddits", subreddits);
-                            firstCheck = true;
                         }
-                        if (done == todo) {
+                        if (done === todo) {
                             setTimeout(() => {
                                 updateStatus();
                             }, 10000);
                             console.log("FINISHED CHECK (or close enough to) - num " + checkCounter);
-                            return;
                         }
                     } catch {
                         console.log("Something broke! We're probably getting blocked...");
                         stop();
-                        return;
                     }
                 }).catch(function (err) {
                     console.log("Request failed! We're probably getting blocked... - " + err);
                     stop();
-                    return;
                 });
             }, delay);
             delay += 1;
