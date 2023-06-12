5094bf5e2ecbfa991381f528e9e6d68e3abebf8a
Max DeVos <max@maxdevos.com>
2023-06-12 14:25:59 -0500

    removed local config.js from git

--- config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    port: 3212,
-    prod: false,
-    logDataOnFail: true,
-    blockedSubreddits: ["r/bi_irl", "r/suddenlybi", "r/ennnnnnnnnnnnbbbbbby", "r/feemagers", "r/BrexitAteMyFace"],
-    url: "https://reddark.untone.uk/"
-}
